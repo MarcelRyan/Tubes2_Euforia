16e0b0a2fa13a6922f6400627b1d40a4726ea88a
Enliven26 <16521443@mahasiswa.itb.ac.id>
2023-03-14 23:25:46 +0700

    map validation

--- src/IO/IO.cs
+++ src/IO/IO.cs
@@ -20,11 +20,27 @@ class IO
     }
     static string[][] ReadMapFile(string fileName, string path = "../../test/",string extension = ".txt")
     {
+        string validChars = "KTRX";
 
         string[] lines = File.ReadAllLines(Path.GetFullPath(path + FixFileExtension(fileName)));
 
-        var map = lines.Select(line => line.Split(' ')).ToArray();
+        var map = lines.Select(line => line.ToUpper().Split(' ')).ToArray();
 
+        if (map.Length == 0) throw new Exception("Map kosong!");
+
+        int columns = map[0].Length;
+
+        foreach(var line in map)
+        {
+            if (line.Length != columns) throw new Exception("Masukan pada setiap baris harus memiliki jumlah karakter yang sama!");
+
+            foreach(var c in line)
+            {
+                if (c.Length > 1) new Exception("Setiap karakter harus dipisah spasi!");
+                
+                if (!validChars.Contains(c)) throw new Exception("Masukan map memiliki karakter yang tidak valid! Karakter yang diperbolehkan adalah K, T, R, dan X");
+            }
+        }
         return map;
     }
 
