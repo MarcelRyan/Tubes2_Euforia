c58dcbadbfef9d3249f942f05df7a5b3cfb33ed5
Enliven26 <16521443@mahasiswa.itb.ac.id>
2023-03-17 18:41:32 +0700

    dfs and bfs parent class

--- src/Algorithm/DFSState.cs
+++ src/Algorithm/DFSState.cs
@@ -2,150 +2,35 @@ using System;
 using System.Collections;
 using System.Linq;
 
-class DFSState
+class DFSState: MazeState
 {
-    public Tuple<int, int> position { get; private set; }
-    public int nodeCount { get; private set; }
-    public int stepCount { get; private set; }
-    public string[][] map { get; private set; }
-    public int row { get; private set; }
-    public int col { get; private set; }
-    public bool foundAll { get; private set; }
-    public bool tspMode { get; private set; }
-    public bool stop { get; private set; }
-    public int treasureCount { get; private set; }
-    public int foundTreasureCount { get; private set; }
 
-    private bool sequentialMode;
-
-    private Tuple<int, int> initialPosition;
-
-    private Tuple<bool, Tuple<int, int>>[,] _checkMap;
-
-    public Stack _stack; 
-
-    static Tuple<bool, Tuple<int, int>> defaultCheckValue = new Tuple<bool, Tuple<int, int>>(false, new Tuple<int, int>(-1, -1));
-
-    // arah terbawah menunjukkan arah pertama yang dieksekusi pada DFS (prinsip stack)
-    static Tuple<int, int>[] directions = new Tuple<int, int>[4] {
-        new Tuple<int, int>(0, -1),
-        new Tuple<int, int>(1, 0),
-        new Tuple<int, int>(0, 1),
-        new Tuple<int, int>(-1, 0),
-    };
+    private Stack _stack; 
 
     // konfigurasi default objek
-    private void defaultConfig()
+    protected override void DefaultConfig()
     {
 
-        position = new Tuple<int, int>(-1, -1);
-        nodeCount = 0;
-        stepCount = 0;
-        treasureCount = 0;
-        foundTreasureCount = 0;
-        stop = false;
-        foundAll = false;
-        row = map.Length;
-        col = map[0].Length;
-        _checkMap = new Tuple<bool, Tuple<int, int>>[row, col];
-
-        for (int i = 0; i < row; i++)
-        {
-            for (int j = 0; j < col; j++)
-            {
-                if (map[i][j] == "K") initialPosition = new Tuple<int, int>(i, j);
-
-                if (map[i][j] == "T") treasureCount++;
-
-                _checkMap[i, j] = defaultCheckValue;
-            }
-        }
+        base.DefaultConfig();
         _stack = new Stack();
         _stack.Push(new Tuple<Tuple<int, int>, Tuple<int, int>>(initialPosition, position));
     }
 
-    // cek apakah posisi saat ini valid
-    private bool isValid(Tuple<int, int> target)
-    {
-        if (target.Item1 < 0 || target.Item2 < 0 || target.Item1 >= row || target.Item2 >= col
-            || GetMapElmt(target) == "X"
-            || GetCheckMap(target).Item1
-           )
-        {
-            return false;
-        }
-
-        return true;
-    }
-
     // solusi ditemukan
-    private void Terminate()
+    protected override void Terminate()
     {
-        stop = true;
+        base.Terminate();
         _stack.Clear();
     }
 
-    private string GetMapElmt(Tuple<int, int> p)
-    {
-        return map[p.Item1][p.Item2];
-    }
-
-    private Tuple<bool, Tuple<int, int>> GetCheckMap(Tuple<int, int> p)
-    {
-        return _checkMap[p.Item1, p.Item2];
-    }
-
-    private void SetCheckMap(Tuple<int, int> p, Tuple<bool, Tuple<int, int>> val)
-    {
-        _checkMap[p.Item1, p.Item2] = val;
-    }
-
     // ctor
-    public DFSState(string[][] map, bool tspMode, bool sequentialMode = false)
+    public DFSState(string[][] map, bool tspMode, bool sequentialMode = false) : base(map, tspMode, sequentialMode)
     {
-        this.map = map;
-        this.tspMode = tspMode;
-        this.sequentialMode = sequentialMode;
-        defaultConfig();
     }
 
-    // mengembalikan matriks visited
-    public bool[,] GetVisited()
-    {
-        bool[,] visited = new bool[row, col];
-
-        for (int i = 0; i < row; i++)
-        {
-            for (int j = 0; j < col; j++)
-            {
-                visited[i, j] = _checkMap[i, j].Item1;
-            }
-        }
-
-        return visited;
-    }
-
-    // mengembalikan path dari Krusty Crab ke posisi saat ini
-    public ArrayList GetCurrentPath()
-    {
-        ArrayList path = new ArrayList();
-
-        Tuple<int, int> tempPosition = position;
-
-        while (tempPosition.Item1 != -1 && tempPosition.Item2 != -1)
-        {
-            path.Add(tempPosition);
-
-            tempPosition = GetCheckMap(tempPosition).Item2;
-        }
-
-        path.Reverse();
-
-        return path;
-    }
 
     // backtrack hingga ada node yang memiliki tetangga yang belum dikunjungi
-    private void BackTrack()
+    protected override void BackTrack()
     {
         if (_stack.Count == 0) return;
 
@@ -174,11 +59,11 @@ class DFSState
     }
 
     // Berpindah satu langkah dengan pendekatan DFS
-    public void Move()
+    public override void Move()
     {
         if (stop) return;
 
-        while (_stack.Count > 0 && !isValid(((Tuple<Tuple<int, int>, Tuple<int, int>>)_stack.Peek()).Item1))
+        while (_stack.Count > 0 && !IsValid(((Tuple<Tuple<int, int>, Tuple<int, int>>)_stack.Peek()).Item1))
         {
             _stack.Pop();
         }
@@ -247,20 +132,4 @@ class DFSState
                         position));
         }
     }
-
-    public void AutoComplete()
-    {
-        // loop sampai tujuan dicapai
-        while (!stop)
-        {
-            Move();
-        }
-    }
-
-    public void Reset()
-    {
-        // reset ke posisi awal
-        defaultConfig();
-    }
-
 }
---
+++ src/Algorithm/MazeState.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Collections;
+using System.Linq;
+
+abstract class MazeState
+{
+    public Tuple<int, int> position { get; protected set; }
+    public int nodeCount { get; protected set; }
+    public int stepCount { get; protected set; }
+    public string[][] map { get; protected set; }
+    public int row { get; protected set; }
+    public int col { get; protected set; }
+    public bool foundAll { get; protected set; }
+    public bool tspMode { get; protected set; }
+    public bool stop { get; protected set; }
+    public int treasureCount { get; protected set; }
+    public int foundTreasureCount { get; protected set; }
+
+    protected bool sequentialMode;
+
+    protected Tuple<int, int> initialPosition;
+
+    protected Tuple<bool, Tuple<int, int>>[,] _checkMap;
+
+    static protected Tuple<bool, Tuple<int, int>> defaultCheckValue = new Tuple<bool, Tuple<int, int>>(false, new Tuple<int, int>(-1, -1));
+
+    // arah terbawah menunjukkan arah pertama yang dieksekusi pada DFS (prinsip stack)
+    // iterasi arah sebaliknya untuk BFS (prinsip queue)
+    static protected Tuple<int, int>[] directions = new Tuple<int, int>[4] {
+        new Tuple<int, int>(0, -1),
+        new Tuple<int, int>(1, 0),
+        new Tuple<int, int>(0, 1),
+        new Tuple<int, int>(-1, 0),
+    };
+
+    // konfigurasi default objek
+    virtual protected void DefaultConfig()
+    {
+
+        position = new Tuple<int, int>(-1, -1);
+        nodeCount = 0;
+        stepCount = 0;
+        treasureCount = 0;
+        foundTreasureCount = 0;
+        stop = false;
+        foundAll = false;
+        row = map.Length;
+        col = map[0].Length;
+        _checkMap = new Tuple<bool, Tuple<int, int>>[row, col];
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                if (map[i][j] == "K") initialPosition = new Tuple<int, int>(i, j);
+
+                if (map[i][j] == "T") treasureCount++;
+
+                _checkMap[i, j] = defaultCheckValue;
+            }
+        }
+    }
+
+    // cek apakah posisi saat ini valid
+    protected bool IsValid(Tuple<int, int> target)
+    {
+        if (target.Item1 < 0 || target.Item2 < 0 || target.Item1 >= row || target.Item2 >= col
+            || GetMapElmt(target) == "X"
+            || GetCheckMap(target).Item1
+           )
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    // solusi ditemukan
+    virtual protected void Terminate()
+    {
+        stop = true;
+    }
+
+    protected string GetMapElmt(Tuple<int, int> p)
+    {
+        return map[p.Item1][p.Item2];
+    }
+
+    protected Tuple<bool, Tuple<int, int>> GetCheckMap(Tuple<int, int> p)
+    {
+        return _checkMap[p.Item1, p.Item2];
+    }
+
+    protected void SetCheckMap(Tuple<int, int> p, Tuple<bool, Tuple<int, int>> val)
+    {
+        _checkMap[p.Item1, p.Item2] = val;
+    }
+
+    // ctor
+    public MazeState(string[][] map, bool tspMode, bool sequentialMode = false)
+    {
+        this.map = map;
+        this.tspMode = tspMode;
+        this.sequentialMode = sequentialMode;
+        DefaultConfig();
+    }
+
+    // mengembalikan matriks visited
+    public bool[,] GetVisited()
+    {
+        bool[,] visited = new bool[row, col];
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                visited[i, j] = _checkMap[i, j].Item1;
+            }
+        }
+
+        return visited;
+    }
+
+    // mengembalikan path dari Krusty Crab ke posisi saat ini
+    public ArrayList GetCurrentPath()
+    {
+        ArrayList path = new ArrayList();
+
+        Tuple<int, int> tempPosition = position;
+
+        while (tempPosition.Item1 != -1 && tempPosition.Item2 != -1)
+        {
+            path.Add(tempPosition);
+
+            tempPosition = GetCheckMap(tempPosition).Item2;
+        }
+
+        path.Reverse();
+
+        return path;
+    }
+
+    // backtrack hingga ada node yang memiliki tetangga yang belum dikunjungi
+    abstract protected void BackTrack();
+
+    // Berpindah satu langkah dengan pendekatan tertentu
+    abstract public void Move();
+   
+    public void AutoComplete()
+    {
+        // loop sampai tujuan dicapai
+        while (!stop)
+        {
+            Move();
+        }
+    }
+
+    public void Reset()
+    {
+        // reset ke posisi awal
+        DefaultConfig();
+    }
+
+}
