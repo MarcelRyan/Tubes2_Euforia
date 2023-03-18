62928a36c104c9dda364ce6ad47edece6d1842e8
MarcelRyan <marcel.ryan2004@gmail.com>
2023-03-18 17:40:40 +0700

    add GUI and test case

---
+++ src/GUI/Helper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.IO;
+
+namespace GUI
+{
+    class Helper
+    {
+        public static string file;
+        public static DataTable TableDataFromTextFile(string location, char delimiter = ' ')
+        {
+            DataTable result;
+            location = file;
+            string[] LineArray = File.ReadAllLines(location);
+            result = FromDataTable(LineArray, delimiter);
+            return result;
+        }
+
+        private static DataTable FromDataTable(string[] LineArray, char delimiter)
+        {
+            DataTable dt = new DataTable();
+
+            AddColumnToTable(LineArray, delimiter, ref dt);
+            AddRowToTable(LineArray, delimiter, ref dt);
+            return dt;
+        }
+
+        private static void AddRowToTable(string[] valueCollection, char delimiter, ref DataTable dt)
+        {
+            for (int i = 0; i < valueCollection.Length; i++)
+            {
+                string[] values = valueCollection[i].Split(delimiter);
+                DataRow dr = dt.NewRow();
+                for (int j = 0; j < values.Length; j++)
+                {
+                    if (values[j] == "K")
+                    {
+                        dr[j] = "Start";
+                    }
+                    else if (values[j] == "R")
+                    {
+                        dr[j] = "  ";
+                    }
+                    else if (values[j] == "X")
+                    {
+                        dr[j] = 0;
+                    }
+                    else if (values[j] == "T")
+                    {
+                        dr[j] = "Treasure";
+                    }
+                }
+                dt.Rows.Add(dr);
+            }
+        }
+
+        private static void AddColumnToTable(string[] columnCollection, char delimiter, ref DataTable dt)
+        {
+            string[] columns = columnCollection[0].Split(delimiter);
+            foreach (string columnName in columns)
+            {
+                DataColumn dc = new DataColumn("", typeof(string));
+                dt.Columns.Add(dc);
+            }
+        }
+    }
+}
---
+++ src/GUI/Treasure_Hunt_Solver.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class Treasure_Hunt_Solver : Form
+    {
+        public Treasure_Hunt_Solver()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Treasure_Hunt_Solver_Load(object sender, EventArgs e)
+        {
+            textBox2.Enabled = false;
+            label7.Enabled = false;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == false)
+            {
+                textBox2.Enabled = false;
+                label7.Enabled = false;
+            }
+            else
+            {
+                textBox2.Enabled = true;
+                label7.Enabled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                Title = "Browse Text File",
+                CheckFileExists = true,
+                CheckPathExists = true,
+                DefaultExt = "txt",
+                Filter = "txt files (*.txt)|*.txt",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+                ReadOnlyChecked = true,
+                ShowReadOnly = true,
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                Helper.file = textBox1.Text;
+                dataGridView1.DataSource = Helper.TableDataFromTextFile(textBox1.Text);
+                textBox1.Text = openFileDialog1.SafeFileName;
+                dataGridView1.CellFormatting += dataGridView1_CellFormatting;
+                dataGridView1.ScrollBars = ScrollBars.None;
+                dataGridView1.RowsDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                int tinggi = dataGridView1.Height / dataGridView1.Rows.Count;
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    row.Height = tinggi;
+                }
+            }
+        }
+
+        private void dataGridView1_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    if (e.RowIndex == i & e.ColumnIndex == j & e.Value.ToString() == "0")
+                    {
+                        e.CellStyle.BackColor = Color.Black;
+                    }
+
+                }
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
