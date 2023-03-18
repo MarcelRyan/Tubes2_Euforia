76612cf784c1dbc592072bfa7389876b8d229105
Enliven26 <16521443@mahasiswa.itb.ac.id>
2023-03-18 22:17:34 +0700

    windowsform project

---
+++ src/Testing.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using System.Linq;
+
+class Testing
+{
+
+    static void MainTestingFunc(string[] args)
+    {
+        string[][] map = FileIO.ReadMapFile("test");
+        // DFSState dfsState = new DFSState(map, true);
+        BFSState bfsState = new BFSState(map, true);
+
+        foreach (var line in map)
+        {
+            foreach (var c in line)
+            {
+                Console.Write(c + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        while (!bfsState.stop)
+        {
+            bfsState.Move();
+            Console.WriteLine(bfsState.position);
+            Console.WriteLine(bfsState.foundTreasureCount);
+            //Console.WriteLine("stack top:" + (Tuple<Tuple<int, int>, Tuple<int, int>>)dfsState._stack.Peek());
+            string temp =  Console.ReadLine();
+            
+        }
+    }
+}
